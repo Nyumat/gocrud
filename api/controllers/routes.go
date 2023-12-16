a9665c7bc447298f036cf39edc42744521fea7ac
Thomas <nyumat18@gmail.com>
2023-12-15 19:27:37 -0800

    ⚡️ feat: login controller

--- api/controllers/routes.go
+++ api/controllers/routes.go
@@ -8,6 +8,7 @@ func (s *Server) initializeRoutes() {
 	s.Router.HandleFunc("/", middlewares.SetMiddlewareJSON(s.Home)).Methods("GET")
 
 	// Login Route
+	s.Router.HandleFunc("/login", middlewares.SetMiddlewareJSON(s.Login)).Methods("POST")
 
 	//Users routes
 	s.Router.HandleFunc("/users", middlewares.SetMiddlewareJSON(s.CreateUser)).Methods("POST")

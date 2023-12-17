408ac98a78dc0b6cab660b3e8310ef1a67a12f20
Thomas <nyumat18@gmail.com>
2023-12-16 19:27:39 -0800

    🐳 feat: setup for dockerized api

--- api/server.go
+++ api/server.go
@@ -12,12 +12,19 @@ import (
 
 var server = controllers.Server{}
 
+func init() {
+	// loads values from .env into the system
+	if err := godotenv.Load(); err != nil {
+		log.Print("sad .env file found")
+	}
+}
+
 func Run() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatalf("Error loading .env file: %v", err)
+		log.Fatalf("Error getting env, %v", err)
 	} else {
-		fmt.Println("Loading env values...")
+		fmt.Println("We are getting the env values")
 	}
 
 	server.Initialize(os.Getenv("DB_DRIVER"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_PORT"), os.Getenv("DB_HOST"), os.Getenv("DB_NAME"))
@@ -25,4 +32,5 @@ func Run() {
 	seed.Load(server.DB)
 
 	server.Run(":8080")
+
 }
